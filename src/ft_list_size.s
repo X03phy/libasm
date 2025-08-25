a5b28be1142e7862ec387bc31de45bb7d3d861b9
Elio Bonutto <elio.bonutto@gmail.com>
2025-08-25 12:48:54 +0200

    feat: add function ft_liste_size()

---
+++ src/ft_list_size.s
@@ -0,0 +1,19 @@
+; int ft_lstsize(t_list *lst);
+
+section .text
+	global ft_lstsize
+
+ft_lstsize:
+	xor     eax, eax
+
+.loop:
+	test    rdi, rdi
+	jz      .done
+
+	inc     eax
+
+	mov     rdi, [rdi + 8] ; lst = lst->next
+	jmp     .loop
+
+.done:
+	ret
