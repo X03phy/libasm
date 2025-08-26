e35079dbe18bc41c7d68cfded36ca232016df00f
Elio Bonutto <elio.bonutto@gmail.com>
2025-08-26 17:32:50 +0200

    feat: add prototypes

--- include/libasm.h
+++ include/libasm.h
@@ -9,12 +9,17 @@ typedef struct s_list
 	struct s_list *next;
 } t_list;
 
-int     ft_atoi_base( const char *s, const char *base );
 size_t  ft_strlen( const char *s );
 char    *ft_strcpy( char *dest, const char *src );
 int     ft_strcmp( const char *s1, const char *s2 );
 ssize_t ft_write( int fd, const void *buf, size_t count );
 ssize_t ft_read( int fd, void *buf, size_t count );
 char    *ft_strdup( const char *s );
+
+int     ft_atoi_base( const char *s, const char *base );
+void    ft_list_push_front( t_list **lst, void *value );
+int     ft_list_size( t_list *lst );
+void    ft_list_sort( t_list **begin_list, int (*cmp)() );
+
 
 #endif
