46f99d5290e05bfda05eef2dc4df67c2292c2cb8
Elio Bonutto <elio.bonutto@gmail.com>
2025-08-26 17:32:25 +0200

    fix: still fixing it

--- src/ft_list_sort.s
+++ src/ft_list_sort.s
@@ -11,39 +11,64 @@ ft_list_sort:
 	test    rsi, rsi       ; cmp == NULL
 	jz      .done
 
-	mov     rax, [rdi]
+	mov     rax, [rdi]     ; *begin_list == NULL
 	test    rax, rax
 	jz      .done
 
+	push    rbx ; swapped
+	push    r12 ; ptr
+	push    r13 ; ptr->next
+	push    r14 ; end
+
+	xor     r14, r14 ; initialize end to NULL
+
 .outer_loop:
-	xor     bl, bl   ; swapped = 0
-	mov     rcx, [rdi] ; ptr = *begin_list
+	xor     bl, bl     ; swapped = 0
+	mov     r12, [rdi] ; ptr = *begin_list
 
 .inner_loop:
-	mov     rdx, [rcx + 8]  ; ptr->next != 0
-	test    rdx, rdx
-	jz      .inner_loop_end
 
-	push rdi      ; save begin_list
-	push rsi      ; save cmp()
+	mov     r13, [r12 + 8]  ; ptr->next
+	cmp     r13, r14        ; check if ptr->next == end
+	je      .outer_loop_inc ; end of the inner loop
 
-	mov  rax, rsi
+	push    rdi
+	push    rsi
 
-	mov  rdi, [rdi]
-	mov  rsi, rax
+	mov     rcx, rsi
+	mov     rdi, [r12] ; ptr->data
+	mov     rsi, [r13] ; ptr->next->data
+	call    rcx
 
+	pop     rsi
+	pop     rdi
 
-	cmp    rax, 0
+	cmp     rax, 0
 	jle    .inner_loop_inc
 
-	swap
+	; swap
+	mov     rax, [r12]
+	mov     rcx, [r13]
 
-.inner_loop_inc
+	mov     [r12], rcx ; ptr->data = ptr->next->data
+	mov     [r13], rax ; ptr->next->data = tmp
 
+	mov     bl, 1
 
-inner_loop_end:
-	test   bl, bl
-	jz .done
+.inner_loop_inc:
+	mov     r12, r13
+	jmp     .inner_loop
+
+.outer_loop_inc:
+	mov     r14, r12
+	test    bl, bl
+	jnz     .outer_loop
+
+	pop     r14
+	pop     r13
+	pop     r12
+	pop     rbx
 
 .done
+	mov   rax, rdi
 	ret
