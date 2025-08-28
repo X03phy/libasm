c202c72df958b07af130c690ecc720ebb29c96c2
Elio Bonutto <elio.bonutto@gmail.com>
2025-08-28 17:17:34 +0200

    fix: norm

--- include/libasm.h
+++ include/libasm.h
@@ -5,8 +5,8 @@
 
 typedef struct s_list
 {
-	void          *data;
-	struct s_list *next;
+	void            *data;
+	struct s_list   *next;
 } t_list;
 
 size_t  ft_strlen( const char *s );
@@ -20,5 +20,7 @@ int     ft_atoi_base( const char *s, const char *base );
 void    ft_list_push_front( t_list **lst, void *value );
 int     ft_list_size( t_list *lst );
 void    ft_list_sort( t_list **begin_list, int (*cmp)() );
+void    ft_list_remove_if( t_list **begin_list, void *data_ref,
+							int (*cmp)(), void (*free_fct)(void *) );
 
 #endif
